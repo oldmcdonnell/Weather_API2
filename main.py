f0f3bcc20064e566dd56596f482ce0b6eb053b32
oldmc <oldmcdonnell@gmail.com>
2023-12-26 12:44:01 -0500

    more station info on home

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 app = Flask(__name__)
 
 stations = pd.read_csv('data/stations.txt', skiprows=17)
-
+stations = stations[["STAID", "STANAME                                 "]]
 @app.route("/")
 def home():
     return render_template("home.html", data=stations.to_html())
